cc154fbd59aaf5d65007042b9ac374a84eb81788
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-03 00:24:12 +0800

    Create main.m

---
+++ 分类/分类/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  分类
+//
+//  Created by Pinna on 2020/7/3.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
