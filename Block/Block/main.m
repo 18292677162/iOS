e2f91ee55fc78962f61b4e12552792115baab8ce
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-04 18:10:49 +0800

    Update main.m

--- Block/Block/main.m
+++ Block/Block/main.m
@@ -1,17 +1,14 @@
 //
 //  main.m
-//  Category
+//  Block
 //
-//  Created by Pinna on 2020/7/3.
+//  Created by Pinna on 2020/7/4.
 //  Copyright © 2020 Pinna. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
     return 0;
 }
