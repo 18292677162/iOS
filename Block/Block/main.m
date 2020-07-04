c161f0f016578fcc48f7679ce8e3ba6f33743e3c
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-04 21:58:56 +0800

    Update main.m

--- Block/Block/main.m
+++ Block/Block/main.m
@@ -8,7 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
+typedef int (^calculte) (int, int);
+
 int main(int argc, const char * argv[]) {
+    /*
+    int (^sumBlock) (int, int);
+    sumBlock = ^(int val1, int val2){
+        return val1 + val2;
+    };
+    NSLog(@"sum = %i", sumBlock(10, 20));
+     */
     
+    
+    calculte sumBlock = ^(int v1, int v2){
+        return v1 + v2;
+    };
+    NSLog(@"sum = %i", sumBlock(10, 20));
+    
+    calculte minusBlock = ^(int v1, int v2){
+        return v1 - v2;
+    };
+    NSLog(@"minus = %i", minusBlock(10, 20));
     return 0;
 }
