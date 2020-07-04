d07d198b23af03970e32d540b9877aed352b7ef0
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-04 23:25:24 +0800

    Create main.m

---
+++ Protocol/Protocol/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Protocol
+//
+//  Created by Pinna on 2020/7/4.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
