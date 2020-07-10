e3cf569ec807e5bf7c0e5154226d3d3b28756bf4
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-10 22:01:37 +0800

    Create SceneDelegate.h

---
+++ 拳皇加载动画/拳皇加载动画/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  拳皇加载动画
+//
+//  Created by Pinna on 2020/7/7.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
