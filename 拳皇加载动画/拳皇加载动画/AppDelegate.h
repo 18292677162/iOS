5efe083526c8e9d4c9f60493e871864ba157efab
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-10 22:01:08 +0800

    Create AppDelegate.h

---
+++ 拳皇加载动画/拳皇加载动画/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  拳皇加载动画
+//
+//  Created by Pinna on 2020/7/7.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
