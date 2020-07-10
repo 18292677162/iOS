77e1a83074251a84be17a3b40dacddd4043b49ce
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-10 22:01:24 +0800

    Create main.m

---
+++ 拳皇加载动画/拳皇加载动画/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  拳皇加载动画
+//
+//  Created by Pinna on 2020/7/7.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
