0fcd831cfbdfa865d651bb40d770b05aa4642aca
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-13 02:26:56 +0800

    Update main.m

--- 代理设计模式/代理设计模式/main.m
+++ 代理设计模式/代理设计模式/main.m
@@ -7,8 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Factory.h"
+#import "Store.h"
+#import "FakeStore.h"
 
 int main(int argc, const char * argv[]) {
+    
+    Factory *factory = [[Factory alloc] init];
+    Store *store = [[Store alloc] init];
+    FakeStore *fakeStore = [[FakeStore alloc] init];
+    
+    factory.store = store;
+    
+    // 虽然遵守了协议，但是当中并没有实现方法，运行会报错，所以一定在调用时检测方法是否实现
+    factory.store = fakeStore;
+    
+    factory.Goods = 10;
+    NSLog(@"货物总数:%d", factory.Goods);
+    
+    [factory factorySell];
+    
+    // [store storeSell:factory];
     
     return 0;
 }
