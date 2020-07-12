d336511572f53c2611285e30283da25776fa309d
18292677162 <39311101+18292677162@users.noreply.github.com>
2020-07-13 02:26:45 +0800

    Create Factory.m

---
+++ 代理设计模式/代理设计模式/Factory.m
@@ -0,0 +1,25 @@
+//
+//  Factory.m
+//  代理设计模式
+//
+//  Created by Pinna on 2020/7/12.
+//  Copyright © 2020 Pinna. All rights reserved.
+//
+
+#import "Factory.h"
+// #import "Store.h"
+// #import "FakeStore.h"
+
+@implementation Factory
+
+-(void)factorySell{
+    NSLog(@"工厂想卖货");
+    
+    // 通知商场卖货，虽然代理都遵守了协议，但是不一定实现方法
+    // 调用时一定要进行判断，判断商店是否真正能帮你卖货
+    if ([self.store respondsToSelector:@selector(storeSell:)]) {
+        [self.store storeSell:self];
+    }
+}
+
+@end
